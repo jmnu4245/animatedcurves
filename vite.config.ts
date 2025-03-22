9e9007b7c9a0bfac2d464799c6a62aead88cc69f
jmnu4245 <97630332+jmnu4245@users.noreply.github.com>
2025-03-22 13:27:57 +0100

    Update vite.config.ts
    update path

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/https://jmnu4245.github.io/animatedcurves/',
 })
